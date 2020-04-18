5358896f3c4da443a8a2e546f8a477d0a68a082a
Andy Soffer <asoffer@google.com>
2020-04-18 11:48:59 -0400

    Smoother animation by handling keyup and keydown.

--- keys.js
+++ keys.js
@@ -1,6 +1,18 @@
 function initKeyListener(keytable) {
   document.addEventListener('keydown', (event) => {
-    const f = keytable[event.keyCode];
+    const mapForKey = keytable[event.keyCode];
+    if (mapForKey === undefined) return;
+    const f = mapForKey.keydown;
+    if (f !== undefined) {
+      f();
+      event.preventDefault();
+    }
+  });
+
+  document.addEventListener('keyup', (event) => {
+    const mapForKey = keytable[event.keyCode];
+    if (mapForKey === undefined) return;
+    const f = mapForKey.keyup;
     if (f !== undefined) {
       f();
       event.preventDefault();
--- main.js
+++ main.js
@@ -1,14 +1,19 @@
 const updateIntervalMs = 20;
 const boardWidth = 50;
 const boardHeight = 40;
-const cellSize = 15;
+const cellSize = 12;
 let board = new Board(boardWidth, boardHeight);
 const canvas = document.getElementById('c').getContext('2d');
 
 document.getElementById('c').width = window.innerWidth;
 document.getElementById('c').height = window.innerHeight;
 
+function clamp(num, min, max) {
+  return num <= min ? min : num >= max ? max : num;
+}
+
 let player = {x: 16, y: 8};
+let playerVelocity = {x: 0, y: 0};
 
 function init() {
   const level = new Level([
@@ -60,39 +65,58 @@ function init() {
 
 function run() {
   board.update();
+
+  // Check player new position after the board has been updated but before
+  // drawing.
+  const newPos = {
+    x: clamp(player.x + playerVelocity.x, 0, boardWidth - 1),
+    y: clamp(player.y + playerVelocity.y, 0, boardHeight - 1)
+  };
+  if (board.at(newPos.x, newPos.y).type == CellType.NORMAL) {
+    board.set(player.x, player.y, new Cell(false, CellType.NORMAL));
+    player = newPos;
+    board.set(player.x, player.y, new Cell(true, CellType.PLAYER));
+  }
+
   board.draw({canvas: canvas, cellSize: cellSize});
   setTimeout(run, updateIntervalMs);
 }
 
 initKeyListener({
-  37: () => {
-    const newPos = {x: Math.max(player.x - 1, 0), y: player.y};
-    if (board.at(newPos.x, newPos.y).type != CellType.NORMAL) return;
-    board.setType(player.x, player.y, CellType.NORMAL);
-    player = newPos;
-    board.setType(player.x, player.y, CellType.PLAYER);
+  37: {
+    keydown: () => {
+      playerVelocity = {x: -1, y: 0};
+    },
+    keyup: () => {
+      playerVelocity = {x: 0, y: 0};
+    },
   },
-  38: () => {
-    const newPos = {x: player.x, y: Math.max(player.y - 1, 0)};
-    if (board.at(newPos.x, newPos.y).type != CellType.NORMAL) return;
-    board.setType(player.x, player.y, CellType.NORMAL);
-    player = newPos;
-    board.setType(player.x, player.y, CellType.PLAYER);
+  38: {
+    keydown: () => {
+      playerVelocity = {x: 0, y: -1};
+    },
+    keyup: () => {
+      playerVelocity = {x: 0, y: 0};
+    },
   },
-  39: () => {
-    const newPos = {x: Math.min(player.x + 1, boardWidth - 1), y: player.y};
-    if (board.at(newPos.x, newPos.y).type != CellType.NORMAL) return;
-    board.setType(player.x, player.y, CellType.NORMAL);
-    player = newPos;
-    board.setType(player.x, player.y, CellType.PLAYER);
+  39: {
+    keydown: () => {
+      playerVelocity = {x: 1, y: 0};
+    },
+    keyup: () => {
+      playerVelocity = {x: 0, y: 0};
+    },
   },
-  40: () => {
-    const newPos = {x: player.x, y: Math.min(player.y + 1, boardHeight - 1)};
-    if (board.at(newPos.x, newPos.y).type != CellType.NORMAL) return;
-    board.setType(player.x, player.y, CellType.NORMAL);
-    player = newPos;
-    board.setType(player.x, player.y, CellType.PLAYER);
-  }
+  40: {
+    keydown: () => {
+      console.log("x");
+      playerVelocity = {x: 0, y: 1};
+    },
+    keyup: () => {
+      console.log("y");
+      playerVelocity = {x: 0, y: 0};
+    },
+  },
 });
 
 init();
